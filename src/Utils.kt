09b79d8740318afdf7fd50ef49ea23f9c4504f9f
szabob <szabob@neocore.local>
2024-12-06 10:03:06 +0100

    Add IPos functions

--- src/Utils.kt
+++ src/Utils.kt
@@ -5,11 +5,18 @@ import java.security.MessageDigest
 typealias IPos = Pair<Int, Int>
 
 fun IPos.add(other: IPos) = IPos(first + other.first, second + other.second)
+fun IPos.mul(other: Int) = IPos(first * other, second * other)
+
+fun <T> List<List<T>>.isValidIndex(pos: IPos): Boolean {
+    return pos.first in indices && pos.second in get(pos.first).indices
+}
+
+fun <T> List<List<T>>.get(pos: IPos) = get(pos.first)[pos.second]
 
 /**
  * Reads lines from the given input txt file.
  */
-fun readInput(name: String) = File("resources", name).readLines()
+fun readInput(name: String) = File("src/main/resources", name).readLines()
 
 /**
  * Converts string to md5 hash.
